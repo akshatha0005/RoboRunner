b2ab58f39fd41059c952ec0b8a7d0d75940dec8e
VibhiJha <vaibhavijha@gmail.com>
2016-04-16 02:08:14 -0700

    test implementation of observer pattern

---
+++ observable_test.py
@@ -0,0 +1,68 @@
+import unittest
+ 
+from observable import Observable
+from observer import Observer
+ 
+ 
+class AnObserver(Observer):
+    def __init__(self):
+        self.args = []
+        self.kwargs = {}
+ 
+    def update(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        return
+ 
+ 
+class observerable_test(unittest.TestCase):
+ 
+    def setUp(self):
+        self.observable = Observable()
+ 
+        self.observer1 = AnObserver()
+        self.observer2 = AnObserver()
+        self.observer3 = AnObserver()
+ 
+    def test_remove_all(self):
+        self.observable.register(self.observer1)
+        self.observable.register(self.observer2)
+        self.observable.register(self.observer3)
+ 
+        self.observable.unregister_all()
+ 
+        # Should be zero registered
+        self.assertEqual(len(self.observable.observers), 0)
+ 
+    def test_register(self):
+        self.observable.register(self.observer1)
+        self.observable.register(self.observer2)
+        self.observable.register(self.observer3)
+ 
+        # Should be three registered
+        self.assertEqual(len(self.observable.observers), 3)
+        self.observable.unregister_all()
+ 
+    def test_unregister(self):
+        self.observable.register(self.observer1)
+        self.observable.register(self.observer2)
+        self.observable.register(self.observer3)
+ 
+        self.observable.unregister(self.observer2)
+        self.observable.unregister(self.observer3)
+ 
+        # Should be one registered
+        self.assertEqual(len(self.observable.observers), 1)
+        self.observable.unregister_all()
+ 
+    def test_update_observers(self):
+        self.observable.register(self.observer1)
+ 
+        self.observable.update_observers('abc', msg='123')
+        self.assertEqual(self.observer1.args[0], 'abc')
+        self.assertEqual(self.observer1.kwargs['msg'], '123')
+        self.observable.unregister_all()
+ 
+ 
+if __name__ == "__main__":
+    unittest.main()
