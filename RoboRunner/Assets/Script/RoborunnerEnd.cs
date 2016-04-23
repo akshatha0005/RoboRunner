ba4c0962b474ff9885ab0f71a7b7ed6441796e81
VibhiJha <vaibhavijha@gmail.com>
2016-04-23 00:22:32 -0700

    Stop game script

---
+++ RoboRunner/Assets/Script/RoborunnerEnd.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoborunnerEnd
+{
+	//Save all the points collected and time of run
+    public int SaveState()
+    {
+        //Return points collected and run time
+        return ObjOfRobo;
+    }
+
+    public void exitGame()
+    {
+        //kill all the objects created, reset everything to 0
+        //Display the saved state on the screen until window is killed
+
+    }
+
+}
